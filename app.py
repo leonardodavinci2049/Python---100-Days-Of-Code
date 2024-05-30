c1916fe9e5b7d6b471b1dd1b1fc6ec012f729b13
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2024-05-30 18:27:16 -0300

    debugging practice

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
-print("Welcome to the Band Name Generator")
+# Fix the code below to make it work
+# The code should print the following:
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the "+" sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
