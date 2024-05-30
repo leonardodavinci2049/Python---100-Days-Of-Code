f54c4542d6b8968848abe0f5b21401c29bcf058b
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2024-05-30 20:06:33 -0300

    day onde finish

---
+++ exer01.py
@@ -0,0 +1,5 @@
+# Python Variables
+name = input("What is your name?")
+print("Hello " + name)
+lenght = len(name)
+print(lenght)
---
+++ main.py
@@ -0,0 +1,3 @@
+# input() will get user input in console
+# then print() will print the word "Hello" and the user input
+print("Hello " + input("Whait is your name?"))
